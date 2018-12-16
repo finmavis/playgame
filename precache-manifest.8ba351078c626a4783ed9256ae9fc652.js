f46cc1e0d3b358be2d62ee3cf4b7c8b486e2594c
Fin Mavis <fin.mavis@yahoo.com>
2018-12-16 14:34:33 +0700

    Updates

---
+++ precache-manifest.8ba351078c626a4783ed9256ae9fc652.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = [
+  {
+    "revision": "26a5026eaf0dbdd75b86a1d49d8349eb",
+    "url": "/playgame/static/media/icon_telegram.26a5026e.svg"
+  },
+  {
+    "revision": "55495d13cdb0e3ebd79f",
+    "url": "/playgame/static/js/main.55495d13.chunk.js"
+  },
+  {
+    "revision": "a4ed9892267e7576de7e",
+    "url": "/playgame/static/js/runtime~main.a4ed9892.js"
+  },
+  {
+    "revision": "35c51dc10640a1f2d32375e65c4d255c",
+    "url": "/playgame/static/media/news-1.35c51dc1.png"
+  },
+  {
+    "revision": "f32544d47fb28f1d2b1975edb62a3dee",
+    "url": "/playgame/static/media/news-2.f32544d4.png"
+  },
+  {
+    "revision": "49d723e9cb31584c038edf13031282f4",
+    "url": "/playgame/static/media/bg-home.49d723e9.png"
+  },
+  {
+    "revision": "6f63045d011f3b5b6a314b76a3fdd8ab",
+    "url": "/playgame/static/media/bg-behind-playgame.6f63045d.png"
+  },
+  {
+    "revision": "bc092bd6b6203c6f04ec9a8274ab93c6",
+    "url": "/playgame/static/media/logo.bc092bd6.svg"
+  },
+  {
+    "revision": "3ed018cf2244a2bf2de10fb71e093c6e",
+    "url": "/playgame/static/media/icon_mail.3ed018cf.svg"
+  },
+  {
+    "revision": "933c5be94de728c459adedd3722ce115",
+    "url": "/playgame/static/media/icon_quote.933c5be9.svg"
+  },
+  {
+    "revision": "c8d02ae5a2e4318def67b21ffe240b5f",
+    "url": "/playgame/static/media/icon_facebook.c8d02ae5.svg"
+  },
+  {
+    "revision": "d0f8f768f1a1d10856ea13340f90d6c7",
+    "url": "/playgame/static/media/icon_youtube.d0f8f768.svg"
+  },
+  {
+    "revision": "f7065526e99baadb8cd9d11558d61e07",
+    "url": "/playgame/static/media/icon_twitter.f7065526.svg"
+  },
+  {
+    "revision": "ce28ab0eba266f879c58f90fe27349ac",
+    "url": "/playgame/static/media/icon_medium.ce28ab0e.svg"
+  },
+  {
+    "revision": "64e16c9fd5087ddfa929",
+    "url": "/playgame/static/js/1.64e16c9f.chunk.js"
+  },
+  {
+    "revision": "e5dd5b543cbf265a81ee992699e9dd40",
+    "url": "/playgame/index.html"
+  }
+];
